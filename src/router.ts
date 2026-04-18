cb62111461ddcde546f93289f7ac7f3ec3906120
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2026-04-18 19:02:09 +0200

    paths

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,9 @@
+import { isLoggedIn } from './store';
+import { renderNavbar } from './components/navbar';
+import { renderHome } from './pages/home';
+import { renderPost } from './pages/post';
+import { renderLogin } from './pages/login';
+import { renderDashboard } from './pages/dashboard';
 interface Jogosultsag {
     bejelentkezesKell: boolean;
 }
@@ -7,3 +13,22 @@ interface UtvonalDefinicio {
     oldal: (params?: Record<string, string>) => void;
     jog?: Jogosultsag;
 }
+const oldalak: UtvonalDefinicio[] = [
+    {
+        utvonal: '#/',
+        oldal: renderHome,
+    },
+    {
+        utvonal: '#/blog/:id',
+        oldal: renderPost,
+    },
+    {
+        utvonal: '#/login',
+        oldal: renderLogin,
+    },
+    {
+        utvonal: '#/dashboard',
+        oldal: renderDashboard,
+        jog: { bejelentkezesKell: true },
+    },
+];
